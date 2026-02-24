608fcb3f525e2219cf4d91c5e9de8c73d23cbd5b
Geokima <Geokima@163.com>
2026-02-24 22:57:31 +0800

    更新流程，补充启动权限申请流程，作为开始

--- Assets/Game/_Scripts/GameManager.cs
+++ Assets/Game/_Scripts/GameManager.cs
@@ -1,6 +1,5 @@
 using Framework.Modules.Http;
 using Framework.Modules.Procedure;
-using Game.Systems;
 using Game.Models;
 using Game.Services;
 using Game.Procedures;
@@ -17,40 +16,47 @@ namespace Game
         public bool IsTestServer = true;
 
         public static GameManager Instance { get; private set; }
-        public static Transform Transform => Instance.transform;
-        public static GameObject GameObject => Instance.gameObject;
+        public static Transform Transform => Instance?.transform;
+        public static GameObject GameObject => Instance?.gameObject;
 
         private void Awake()
         {
-            Instance = this;
+            Instance ??= this;
             GameArchitecture.OnRegisterPatch += architecture =>
             {
                 // Services
-                architecture.RegisterSystem(new LoginService());
+                architecture.RegisterSystem(new AuthService());
+                architecture.RegisterSystem(new PlayerService());
                 architecture.RegisterSystem(new ResourceService());
                 architecture.RegisterSystem(new InventoryService());
 
-                // Systems
-                architecture.RegisterSystem(new LoginSystem());
-
                 // Models
+                architecture.RegisterModel(new AccountModel());
                 architecture.RegisterModel(new PlayerModel());
                 architecture.RegisterModel(new InventoryModel());
             };
+
+            LaunchGame();
+        }
+
+        private void LaunchGame()
+        {
             GameArchitecture.Launch();
+
+            // HttpSystem 配置
             var httpSystem = GameArchitecture.Instance.GetSystem<HttpSystem>();
             httpSystem.SetConfig(ProductionServerUrl, TestServerUrl, IsTestServer);
 
-            // Procedures
+            // Procedures 注册
             var procedureSystem = GameArchitecture.Instance.GetSystem<ProcedureSystem>();
+            procedureSystem.RegisterProcedure(new LaunchProcedure());
             procedureSystem.RegisterProcedure(new PreloadProcedure());
             procedureSystem.RegisterProcedure(new LoginProcedure());
             procedureSystem.RegisterProcedure(new MainProcedure());
+
             // 启动流程
             var procedure = GameArchitecture.Instance.GetSystem<ProcedureSystem>();
-            procedure.Start(ProcedureType.Preload);
-
-            Debug.Log("GameManager initialized");
+            procedure.Start<LaunchProcedure>();
         }
 
         private void Update()
